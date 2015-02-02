7fbba6397edafaf2c649e50e5e99f158ea3f09ad
Tim Schmitz <tschmitz@gmail.com>
2015-02-02 02:22:09 -0500

    Make functions public

--- DateIntervalOperators.swift
+++ DateIntervalOperators.swift
@@ -8,62 +8,62 @@
 
 import Foundation
 
-extension Int {
-	func day() -> NSDateComponents {
+public extension Int {
+	public func day() -> NSDateComponents {
 		let components = NSDateComponents()
 		components.day = self
 		return components
 	}
-	func days() -> NSDateComponents { return self.day() }
+	public func days() -> NSDateComponents { return self.day() }
 	
-	func week() -> NSDateComponents {
+	public func week() -> NSDateComponents {
 		let components = NSDateComponents()
 		components.weekOfYear = self
 		return components
 	}
-	func weeks() -> NSDateComponents { return self.week() }
+	public func weeks() -> NSDateComponents { return self.week() }
 	
-	func month() -> NSDateComponents {
+	public func month() -> NSDateComponents {
 		let components = NSDateComponents()
 		components.month = self
 		return components
 	}
-	func months() -> NSDateComponents { return self.month() }
+	public func months() -> NSDateComponents { return self.month() }
 	
-	func year() -> NSDateComponents {
+	public func year() -> NSDateComponents {
 		let components = NSDateComponents()
 		components.year = self
 		return components
 	}
-	func years() -> NSDateComponents { return self.year() }
+	public func years() -> NSDateComponents { return self.year() }
 	
-	func second() -> NSDateComponents {
+	public func second() -> NSDateComponents {
 		let components = NSDateComponents()
 		components.second = self
 		return components
 	}
-	func seconds() -> NSDateComponents { return self.second() }
+	public func seconds() -> NSDateComponents { return self.second() }
 	
-	func minute() -> NSDateComponents {
+	public func minute() -> NSDateComponents {
 		let components = NSDateComponents()
 		components.minute = self
 		return components
 	}
-	func minutes() -> NSDateComponents { return self.minute() }
+	public func minutes() -> NSDateComponents { return self.minute() }
 	
-	func hour() -> NSDateComponents {
+	public func hour() -> NSDateComponents {
 		let components = NSDateComponents()
 		components.hour = self
 		return components
 	}
-	func hours() -> NSDateComponents { return self.hour() }
+	public func hours() -> NSDateComponents { return self.hour() }
 }
 
-func +(lhs: NSDate, rhs: NSDateComponents) -> NSDate {
+public func +(lhs: NSDate, rhs: NSDateComponents) -> NSDate {
 	return NSCalendar.currentCalendar().dateByAddingComponents(rhs, toDate: lhs, options: nil) ?? lhs
 }
 
-func -(lhs: NSDate, rhs: NSDateComponents) -> NSDate {
+public func -(lhs: NSDate, rhs: NSDateComponents) -> NSDate {
 	if rhs.era != Int(NSUndefinedDateComponent) { rhs.era *= -1 }
 	if rhs.year != Int(NSUndefinedDateComponent) { rhs.year *= -1 }
 	if rhs.month != Int(NSUndefinedDateComponent) { rhs.month *= -1 }
